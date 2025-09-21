6a3f07e6338ecbf826c193a856107a632148a3a9
niiyiko <nonkman26@gmail.com>
2025-09-21 01:40:12 -0500

    Fix Submit Partnership Inquiry button navigation

--- src/pages/BuilderPartnerships.tsx
+++ src/pages/BuilderPartnerships.tsx
@@ -14,7 +14,7 @@ const BuilderPartnerships: React.FC = () => {
   }, []);
 
   const handlePartnershipInquiry = () => {
-    navigate('/partners');
+    navigate('/builder-partnerships');
   };
 
   return (
